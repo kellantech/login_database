b9e2f766e730be2d21b787f98719ca53fef6351e
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2022-09-01 16:07:25 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,54 @@
+import sqlite3
+def init():
+	connection = sqlite3.connect("login.db")
+	c = connection.cursor()
+	c.execute("CREATE TABLE IF NOT EXISTS login(un,pwd)")
+
+def does_user_exist(uname):
+	connection = sqlite3.connect("login.db")
+
+	c = connection.cursor()
+	ul = c.execute(f"SELECT * FROM login WHERE un='{uname}'").fetchall()
+	if len(ul) == 0: return 0
+	else: return 1
+		
+def add_user(uname,pwd):
+	connection = sqlite3.connect("login.db")
+	c = connection.cursor()
+	if does_user_exist(uname)==0:
+		qu = f"INSERT INTO login(un,pwd) values ('{escape(uname)}','{escape(pwd)}')"
+		c.execute(qu)
+	connection.commit()
+
+def escape(tx):
+	return tx.replace("\'","").replace("\"",'').replace("-","").replace("<",'')
+	
+def login(uname,pwd):
+	connection = sqlite3.connect("login.db")
+	c = connection.cursor()
+	query = f"""SELECT *
+  FROM login
+ WHERE un = '{escape(uname)}'
+   AND pwd  = '{escape(pwd)}' LIMIT 1
+"""
+	lst = (c.execute(query).fetchall())
+	if lst == []:return 0
+	if lst != []:return 1
+
+def print_db():
+	connection = sqlite3.connect("login.db")
+	c = connection.cursor()
+	query = """SELECT *
+  FROM login"""
+	lst = (c.execute(query).fetchall())
+	print(lst)
+
+def del_user(uname, password):
+	connection = sqlite3.connect("login.db")
+	c = connection.cursor()
+	isdel = 0
+	if login(uname,password) == 1 : 
+		c.execute(f"DELETE FROM login WHERE un='{uname}';");isdel=1
+	connection.commit()
+	if isdel==1: return 1
+	else:return 0
